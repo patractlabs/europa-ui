defce9b20a164dea5e41e4ed760da799775b2297
Zenacia <17864213831@163.com>
2021-06-10 11:35:13 +0800

    extrinsicHex, extrinsicHash

--- src/pages/developer/Extrinsics/Submission.tsx
+++ src/pages/developer/Extrinsics/Submission.tsx
@@ -1,25 +1,21 @@
-import React, { FC, ReactElement, useCallback, useContext, useState } from 'react';
+import React, { FC, ReactElement, useCallback, useContext, useMemo, useState } from 'react';
 import { Button, Col, message, Row } from 'antd';
 import styled from 'styled-components';
-import type { StorageEntry } from '@polkadot/types/primitive/types';
-import type { SubmittableExtrinsicFunction, QueryableStorageEntry } from '@polkadot/api/types';
-import type { StorageEntryTypeLatest } from '@polkadot/types/interfaces';
+import type { SubmittableExtrinsicFunction } from '@polkadot/api/types';
 import type { TypeDef } from '@polkadot/types/types';
 import { ApiRx } from '@polkadot/api';
-import { unwrapStorageType } from '@polkadot/types/primitive/StorageKey';
 import { getTypeDef } from '@polkadot/types';
-import { TypeDefInfo } from '@polkadot/types/types';
 import { GenericCall } from '@polkadot/types';
 import { AccountsContext, ApiContext, handleTxResults } from '../../../core';
 import { Sections } from '../ChainState/Sections';
 import { Methods } from '../ChainState/Methods';
-import AddSvg from '../../../assets/imgs/add.svg';
-import { RawParamOnChangeValue, RawParams } from '../../../react-params/types';
+import { RawParamOnChangeValue } from '../../../react-params/types';
 import Params from '../../../react-params';
-import { AddressInput } from '../../../shared';
+import { AddressInput, Style } from '../../../shared';
 import keyring from '@polkadot/ui-keyring';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { u8aToHex } from '@polkadot/util';
 
 const Wrapper = styled.div`
   padding: 20px;
@@ -30,19 +26,24 @@ const Submit = styled.div`
   justify-content: flex-end;
   margin-top: 10px;
 `;
+const Encoded = styled.div`
+  border: 1px solid ${Style.color.border.default};
+  padding: 6px 8px;
+
+  > .span {
+    color: ${Style.color.label.default};
+  }
+  
+  > .p {
+    color: ${Style.color.label.primary};
+  }
+`;
 
 interface TypeDefExt extends TypeDef {
   withOptionActive?: boolean;
 }
 
 type ParamsType = { type: TypeDefExt }[];
-
-interface KeyState {
-  defaultValues: RawParams | undefined | null;
-  isIterable: boolean;
-  key: QueryableStorageEntry<'rxjs'>;
-  params: ParamsType;
-}
 
 const createOptions = (api: ApiRx): string[] => {
   return Object
@@ -56,58 +57,6 @@ function getParams ({ meta }: SubmittableExtrinsicFunction<'rxjs'>): { name: str
     name: arg.name.toString(),
     type: getTypeDef(arg.type.toString())
   }));
-}
-
-function expandParams (st: StorageEntryTypeLatest, isIterable: boolean): ParamsType {
-  let types: string[] = [];
-
-  if (st.isDoubleMap) {
-    types = [st.asDoubleMap.key1.toString(), st.asDoubleMap.key2.toString()];
-  } else if (st.isMap) {
-    types = [st.asMap.key.toString()];
-  } else if (st.isNMap) {
-    types = st.asNMap.keyVec.map((k) => k.toString());
-  }
-
-  return types.map((str, index) => {
-    let type: TypeDefExt;
-
-    if (isIterable && index === (types.length - 1)) {
-      type = getTypeDef(`Option<${str}>`);
-      type.withOptionActive = true;
-    } else {
-      type = getTypeDef(str);
-    }
-
-    return { type };
-  });
-}
-
-function checkIterable (type: StorageEntryTypeLatest): boolean {
-  // in the case of Option<type> keys, we don't allow map iteration, in this case
-  // we would have option for the iterable and then option for the key value
-  return type.isPlain || (
-    type.isMap
-      ? getTypeDef(type.asMap.key.toString())
-      : type.isDoubleMap
-        ? getTypeDef(type.asDoubleMap.key2.toString())
-        : getTypeDef(type.asNMap.keyVec[type.asNMap.keyVec.length - 1].toString())
-  ).info !== TypeDefInfo.Option;
-}
-
-function expandKey (api: ApiRx, sectionName: string, method: string): KeyState {
-  const key = api.query[sectionName][method];
-  const { creator: { meta: { type }, section } } = key;
-  const isIterable = checkIterable(type);
-
-  return {
-    defaultValues: section === 'session' && type.isDoubleMap
-      ? [{ isValid: true, value: api.consts.session.dedupKeyPrefix.toHex() }]
-      : null,
-    isIterable,
-    key,
-    params: expandParams(type, isIterable)
-  };
 }
 
 const createMethods = (api: ApiRx, sectionName: string) => {
@@ -152,6 +101,25 @@ export const Submission: FC = (): ReactElement => {
   }>(methods[0]);
   const [paramValues, setParamValues] = useState<RawParamOnChangeValue[]>([]);
   const [params, setParams] = useState<ParamsType>(getParams(api.tx[section][method.value]));
+  
+  const [extrinsicHex, extrinsicHash] = useMemo(
+    (): [string, string] => {
+      const values = paramValues.map(p => p.value) as any[];
+      const exec = api.tx[section][method.value];
+      console.log('values', values);
+
+      try {
+        const extrinsic = exec(...values);
+        const u8a = extrinsic.method.toU8a();
+  
+        // don't use the built-in hash, we only want to convert once
+        return [u8aToHex(u8a), extrinsic.registry.hash(u8a).toHex()];
+      } catch (e) {
+        return ['0x', '0x'];
+      }
+    },
+    [api.tx, section, method, paramValues],
+  );
 
   const onUnsignedSubmit = useCallback(async () => {
     const exec = api.tx[section][method.value];
@@ -240,6 +208,14 @@ export const Submission: FC = (): ReactElement => {
         onChange={setParamValues}
         params={params}
       />
+      <Encoded>
+        <span>encoded call data</span>
+        <p>{extrinsicHex}</p>
+      </Encoded>
+      <Encoded>
+        <span>encoded call hash</span>
+        <p>{extrinsicHash}</p>
+      </Encoded>
       <Submit>
         <Button onClick={onUnsignedSubmit}>Submit Unsigned</Button>
         <Button onClick={onSignedSubmit}>Submit Transaction</Button>
