098ddf828756289871c2c0e64ff831350c962768
Zenacia <17864213831@163.com>
2021-07-16 17:31:35 +0800

    up

--- .github/workflows/rename-artifact.js
+++ .github/workflows/rename-artifact.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const dir = path.resolve(__dirname, '../../dist');
-const file = fs.readdirSync(dir).find(file => /^Europa-UI.*((\.AppImage)|(\.exe)|(\.dmg))$/.test(file));
+const file = fs.readdirSync(dir).find(file => /^europa-ui.*((\.AppImage)|(\.exe)|(\.dmg))$/.test(file));
 const newFile = 'europa';
 
 console.log(`rename file: ${file} -> ${newFile}`)
